1da250cb5da35a5fcdb33ef70cf5691aba17adf0
Rafael Aguiar <terule@gmail.com>
2022-09-08 15:44:22 -0300

    Rquisito 1

---
+++ src/redux/store.js
@@ -0,0 +1,15 @@
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/index';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk)),
+);
+
+if (window.Cypress) {
+  window.store = store;
+}
+
+export default store;
